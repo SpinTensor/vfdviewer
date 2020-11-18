0fefaf080bad8d45c36e3083a454a2be82a9c974
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-11-18 12:48:39 +0100

    included stack read in vfd read

--- src/vfd_IO.c
+++ src/vfd_IO.c
@@ -2,12 +2,17 @@
 
 #include "vfd_types.h"
 #include "vfd_header_IO.h"
+#include "vfd_stacks_IO.h"
 
 vfd_t read_vfd_file(FILE *vfd_file) {
    vfd_t vfd;
    vfd.header = read_vfd_header(vfd_file);
 #ifdef _DEBUG
    print_vfd_header(vfd.header);
+#endif
+   vfd.stacks = read_vfd_stacks(vfd_file, vfd.header);
+#ifdef _DEBUG
+   print_vfd_stacks(vfd.stacks, vfd.header);
 #endif
    return vfd;
 }

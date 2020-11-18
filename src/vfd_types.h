2671e69293443ac5941850b07690ccd663be2f49
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-11-18 09:04:21 +0100

    defined types to store vfd information

---
+++ src/vfd_types.h
@@ -0,0 +1,76 @@
+#ifndef VFD_TYPES_H
+#define VFD_TYPES_H
+
+#include <stdbool.h>
+
+#define VFTR_VERSIONSIZE 16
+#define DATESTRINGSIZE 24
+
+typedef struct {
+   int vfd_version;
+   char vftrace_version[VFTR_VERSIONSIZE+1];
+   char date[DATESTRINGSIZE+1];
+   long long interval;
+   int nthreads;
+   int mythread;
+   int nranks;
+   int myrank;
+   double runtime;
+   unsigned int function_samplecount;
+   unsigned int message_samplecount;
+   unsigned int stackscount;
+   long stacksoffset;
+   long sampleoffset;
+   int nhwc;
+} vfd_header_t;
+
+typedef enum {
+   fnct_entry,
+   fnct_exit,
+   mpi_message
+} vfd_sample_kind_enum;
+
+typedef struct vdf_stack_entry_type{
+   int id;
+   int levels;
+   int callerID;
+   struct vfd_stack_entry_type *caller;
+   int ncallees;
+   struct vfd_stack_entry_ttype **callees;
+   bool precise;
+   int namelen;
+   char *name;
+} vfd_stack_entry_t;
+
+typedef struct {
+   int stack_id;
+   long long sampletime;
+} vfd_stack_sample_t;
+
+typedef enum {
+   send,
+   recv
+} vfd_message_dir_enum;
+
+typedef struct {
+   vfd_message_dir_enum dir;
+   int rank;
+   int typeID;
+   int count;
+   int typeSize;
+   int tag;
+   long long tstart;
+   double dtstart;
+   long long tend;
+   double dtend;
+   int callingStackID;
+} vfd_message_t;
+
+typedef struct {
+   vfd_header_t header;
+   vfd_stack_sample_t *stack_samples;
+   vfd_message_t *messages;
+   vfd_stack_entry_t *stacks;
+} vfd_t;
+
+#endif

5874b812e411d5c6e311ab181b1d580d39abcef9
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-11-24 08:28:06 +0100

    enabled reading of vfd files from the cmdline

--- src/vfdviewer.c
+++ src/vfdviewer.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <gtk/gtk.h>
 
 #include "paths.h"
 #include "vgtk_builder.h"
 #include "vgtk_main_window.h"
+#include "vgtk_stack_treeview.h"
 #include "vfd_list.h"
 
 int main(int argc, char **argv) {
@@ -18,9 +20,28 @@ int main(int argc, char **argv) {
    free(gladefile);
    gladefile = NULL;
 
+#ifdef _DEBUG
+         fprintf(stderr, "Reading vfd traces from the command line\n");
+#endif
+   // read in all vfdfiles supplied via the commandline
+   for (int iarg=1; iarg<argc; iarg++) {
+      if (access(argv[iarg], F_OK) != -1) {
+#ifdef _DEBUG
+         fprintf(stderr, "Opening File %s\n", argv[iarg]);
+#endif
+         char *filename = strdup(argv[iarg]);
+         vfd_t *vfdfile = new_vfd(filename);
+         append_vfd(vfdfile);
+         // add the vfdtrace to the stack treeView
+         vgtk_stack_tree_add_vfdtrace(vfdfile);
+      } else {
+         fprintf(stderr, "Unable to locate file %s\n", argv[iarg]);
+      }
+   }
+
+
    vgtk_show_main_window();
    gtk_main();
-
 
    // remove all remaining vfd traces
    remove_and_free_vfd_list();

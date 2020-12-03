3550cdb65448b2ffe8bf8db5f0d582f12d864641
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-12-03 09:04:21 +0100

    defined some default colors

---
+++ src/vgtk_colors.c
@@ -0,0 +1,15 @@
+#include "vgtk_draw_types.h"
+
+const vgtk_color_t vgtk_color_white = {
+   .red = 1.0,
+   .green = 1.0,
+   .blue = 1.0,
+   .alpha = 1.0
+};
+
+const vgtk_color_t vgtk_color_black = {
+   .red = 0.0,
+   .green = 0.0,
+   .blue = 0.0,
+   .alpha = 1.0
+};
---
+++ src/vgtk_colors.h
@@ -0,0 +1,10 @@
+#ifndef VGTK_COLORS
+#define VGTK_COLORS
+
+#include "vgtk_draw_types.h"
+
+extern const vgtk_color_t vgtk_color_white;
+
+extern const vgtk_color_t vgtk_color_black;
+
+#endif

07e0c21ddf1fff3388237e136a8472c2b644983b
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-12-08 07:36:01 +0100

    added a bunch of color definitions

--- src/vgtk_colors.c
+++ src/vgtk_colors.c
@@ -1,15 +1,142 @@
 #include "vgtk_draw_types.h"
 
+#define CNORM 255.0
+
 const vgtk_color_t vgtk_color_white = {
-   .red = 1.0,
-   .green = 1.0,
-   .blue = 1.0,
-   .alpha = 1.0
+   .red = 255.0/CNORM,
+   .green = 255.0/CNORM,
+   .blue = 255.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_silver = {
+   .red = 192.0/CNORM,
+   .green = 192.0/CNORM,
+   .blue = 192.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_gray = {
+   .red = 128.0/CNORM,
+   .green = 128.0/CNORM,
+   .blue = 128.0/CNORM,
+   .alpha = 255.0/CNORM
 };
 
 const vgtk_color_t vgtk_color_black = {
-   .red = 0.0,
-   .green = 0.0,
-   .blue = 0.0,
-   .alpha = 1.0
+   .red = 0.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
 };
+
+const vgtk_color_t vgtk_color_red = {
+   .red = 255.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_maroon = {
+   .red = 128.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_yellow = {
+   .red = 255.0/CNORM,
+   .green = 255.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_olive = {
+   .red = 128.0/CNORM,
+   .green = 128.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_lime = {
+   .red = 0.0/CNORM,
+   .green = 255.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_green = {
+   .red = 0.0/CNORM,
+   .green = 128.0/CNORM,
+   .blue = 0.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_aqua = {
+   .red = 0.0/CNORM,
+   .green = 255.0/CNORM,
+   .blue = 255.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_teal = {
+   .red = 0.0/CNORM,
+   .green = 128.0/CNORM,
+   .blue = 128.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_blue = {
+   .red = 0.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 255.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_navy = {
+   .red = 0.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 128.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_fuchsia = {
+   .red = 255.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 255.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+const vgtk_color_t vgtk_color_purple = {
+   .red = 128.0/CNORM,
+   .green = 0.0/CNORM,
+   .blue = 128.0/CNORM,
+   .alpha = 255.0/CNORM
+};
+
+#define NCOLORS 16
+
+const int vgtk_ncolors = NCOLORS;
+
+vgtk_color_t vgtk_color_list[] = {
+   vgtk_color_white,
+   vgtk_color_silver,
+   vgtk_color_gray,
+   vgtk_color_black,
+   vgtk_color_red,
+   vgtk_color_maroon,
+   vgtk_color_yellow,
+   vgtk_color_olive,
+   vgtk_color_lime,
+   vgtk_color_green,
+   vgtk_color_aqua,
+   vgtk_color_teal,
+   vgtk_color_blue,
+   vgtk_color_navy,
+   vgtk_color_fuchsia,
+   vgtk_color_purple
+};
+
+#undef NCOLORS
+
+#undef CNORM
--- src/vgtk_colors.h
+++ src/vgtk_colors.h
@@ -5,6 +5,39 @@
 
 extern const vgtk_color_t vgtk_color_white;
 
+extern const vgtk_color_t vgtk_color_silver;
+
+extern const vgtk_color_t vgtk_color_gray;
+
 extern const vgtk_color_t vgtk_color_black;
+
+extern const vgtk_color_t vgtk_color_red;
+
+extern const vgtk_color_t vgtk_color_maroon;
+
+extern const vgtk_color_t vgtk_color_yellow;
+
+extern const vgtk_color_t vgtk_color_olive;
+
+extern const vgtk_color_t vgtk_color_lime;
+
+extern const vgtk_color_t vgtk_color_green;
+
+extern const vgtk_color_t vgtk_color_aqua;
+
+extern const vgtk_color_t vgtk_color_teal;
+
+extern const vgtk_color_t vgtk_color_blue;
+
+extern const vgtk_color_t vgtk_color_navy;
+
+extern const vgtk_color_t vgtk_color_fuchsia;
+
+extern const vgtk_color_t vgtk_color_purple;
+
+
+extern const int vgtk_ncolors;
+
+extern vgtk_color_t vgtk_color_list[];
 
 #endif

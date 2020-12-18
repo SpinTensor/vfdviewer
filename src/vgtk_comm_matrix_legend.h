1ed525c2c17360250cf6238347a24f316dadb501
Felix Uhl <Felix.Uhl@emea.nec.com>
2020-12-18 13:04:59 +0100

    started implementation of options regarding other observables for the communication matrix

---
+++ src/vgtk_comm_matrix_legend.h
@@ -0,0 +1,10 @@
+#ifndef VGTK_COMM_MATRIX_LEGEND_H
+#define VGTK_COMM_MATRIX_LEGEND_H
+
+void vgtk_build_comm_matrix_legend(GtkBuilder *builder);
+
+void set_comm_matrix_bw_label_max_value(double value);
+void set_comm_matrix_bw_label_mid_value(double value);
+void set_comm_matrix_bw_label_min_value(double value);
+
+#endif

abcde621a81f9f5ce5eae0e765cf5b47541ef249
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-02-16 08:49:39 +0100

    removed warning on unused variable

--- src/vgtk_hwc_plot_select_observables.c
+++ src/vgtk_hwc_plot_select_observables.c
@@ -17,6 +17,7 @@ void vgtk_build_hwc_select_observables(GtkBuilder *builder) {
 void on_hwc_plot_derived_counters_observablename_entry_activate(
    GtkEntry *entry,
    gpointer  user_data) {
+   (void) user_data;
 
    const char *text = gtk_entry_get_text(entry);
    vgtk_hwc_set_plot_yaxis_title(text);

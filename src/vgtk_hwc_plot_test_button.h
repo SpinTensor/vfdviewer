4e8b689d5f857c71f708440cfdb2386882eb9862
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-02-17 09:11:55 +0100

    remove test replot button

--- src/vgtk_hwc_plot_test_button.h
+++
@@ -1,8 +0,0 @@
-#ifndef VGTK_HWC_PLOT_TEST_BUTTON_H
-#define VGTK_HWC_PLOT_TEST_BUTTON_H
-
-void vgtk_build_hwc_plot_test_button(GtkBuilder *builder);
-
-void on_test_replot_button_clicked(GtkButton *button);
-
-#endif
--- src/vgtk_main_hwc_plot.c
+++ src/vgtk_main_hwc_plot.c
@@ -1,14 +1,12 @@
 #include <gtk/gtk.h>
 
 #include "vgtk_hwc_plot.h"
-#include "vgtk_hwc_plot_test_button.h"
 #include "vgtk_hwc_plot_select_observables.h"
 #include "vgtk_hwc_plot_trace_list.h"
 
 void vgtk_build_main_hwc_plot(GtkBuilder *builder) {
    vgtk_build_hwc_plot(builder);
 
-   vgtk_build_hwc_plot_test_button(builder);
    //vgtk_build_hwc_plot_range_select(builder);
    vgtk_build_hwc_select_observables(builder);
    vgtk_build_hwc_plot_trace_list(builder);
